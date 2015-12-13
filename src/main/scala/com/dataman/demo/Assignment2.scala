f5221aab694e0379f54e4195e0ccca8db298ea2b
wuti1609 <wuti1609@gmail.com>
2015-12-13 19:19:47 +0800

    fix

--- src/main/scala/com/dataman/demo/Assignment2.scala
+++ src/main/scala/com/dataman/demo/Assignment2.scala
@@ -2,6 +2,8 @@ package com.dataman.demo
 
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.types.{StructType,StructField,StringType}
 
 /**
  * Created by mymac on 15/11/3.
@@ -9,12 +11,12 @@ import org.apache.spark.{SparkContext, SparkConf}
 object Assignment2 {
 
   val source = "hdfs://192.168.111.134:9000/Assignment2/jingdong.json"
-  val result = "/tmp/project/data/answer"
 
   def main(args: Array[String]) = {
 
     val conf = new SparkConf().setAppName("assignment2")
     val sc = new SparkContext(conf)
+    val sqlContext = new SQLContext(sc)
       
     //please code here.
 
